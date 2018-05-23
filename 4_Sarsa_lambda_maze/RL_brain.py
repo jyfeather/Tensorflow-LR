e933ceedc9ef6814ac668eaf44bff940f480e53c
Yan Jin <jyfeather@gmail.com>
2018-05-23 11:46:11 -0700

    添加DQN

---
+++ 4_Sarsa_lambda_maze/RL_brain.py
@@ -0,0 +1,85 @@
+import numpy as np
+import pandas as pd
+
+
+class RL(object):
+    def __init__(self, action_space, learning_rate=0.01, reward_decay=0.9, e_greedy=0.9):
+        self.actions = action_space  # a list
+        self.lr = learning_rate
+        self.gamma = reward_decay
+        self.epsilon = e_greedy
+
+        self.q_table = pd.DataFrame(columns=self.actions, dtype=np.float64)
+
+    def check_state_exist(self, state):
+        if state not in self.q_table.index:
+            # append new state to q table
+            self.q_table = self.q_table.append(
+                pd.Series(
+                    [0]*len(self.actions),
+                    index=self.q_table.columns,
+                    name=state,
+                )
+            )
+
+    def choose_action(self, observation):
+        self.check_state_exist(observation)
+        # action selection
+        if np.random.rand() < self.epsilon:
+            # choose best action
+            state_action = self.q_table.loc[observation, :]
+            state_action = state_action.reindex(np.random.permutation(state_action.index))     # some actions have same value
+            action = state_action.idxmax()
+        else:
+            # choose random action
+            action = np.random.choice(self.actions)
+        return action
+
+    def learn(self, *args):
+        pass
+
+
+class SarsaLambdaTable(RL): # 继承 RL Class
+    def __init__(self, actions, learning_rate=0.01, reward_decay=0.9, e_greedy=0.9, trace_decay=0.9):
+        super(SarsaLambdaTable, self).__init__(actions, learning_rate, reward_decay, e_greedy)
+
+        # backward view, eligibility trace.
+        self.lambda_ = trace_decay
+        self.eligibility_trace = self.q_table.copy()
+
+    def check_state_exist(self, state):
+        if state not in self.q_table.index:
+            # append new state to q table
+            to_be_append = pd.Series(
+                    [0] * len(self.actions),
+                    index=self.q_table.columns,
+                    name=state,
+                )
+            self.q_table = self.q_table.append(to_be_append)
+
+            # also update eligibility trace
+            self.eligibility_trace = self.eligibility_trace.append(to_be_append)
+
+    def learn(self, s, a, r, s_, a_):
+        self.check_state_exist(s_)
+        q_predict = self.q_table.loc[s, a]
+        if s_ != 'terminal':
+            q_target = r + self.gamma * self.q_table.loc[s_, a_]  # next state is not terminal
+        else:
+            q_target = r  # next state is terminal
+        error = q_target - q_predict
+
+        # 对于经历过的state-action，我们让它+1，证明它是得到 reward 路途中不可或缺的一环。
+
+        # Method 1:
+        # self.eligibility_trace.loc[s, a] += 1
+
+        # Method 2:
+        self.eligibility_trace.loc[s, :] *= 0
+        self.eligibility_trace.loc[s, a] = 1
+
+        # Q table 更新
+        self.q_table += self.lr * error * self.eligibility_trace
+
+        # 随着时间衰减 eligibility trace 的值，离获取 reward 越远的步，他的不可或缺性越小
+        self.eligibility_trace *= self.gamma*self.lambda_
